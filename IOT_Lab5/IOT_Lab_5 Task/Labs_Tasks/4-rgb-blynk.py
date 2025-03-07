eab8b7305b4c3c4d4e46ccf427f0f369c9eddc64
safder510 <safderrehman510@gmail.com>
2025-03-07 11:39:58 +0500

    Seperate my lab Tasks

--- IOT_Lab5/IOT_Lab_5 Task/Labs_Tasks/4-rgb-blynk.py
+++ IOT_Lab5/IOT_Lab_5 Task/Labs_Tasks/4-rgb-blynk.py
@@ -7,9 +7,9 @@ import utime as time
 from machine import Pin
 from neopixel import NeoPixel
 
-WIFI_SSID = 'your network/hotspot ssid'
-WIFI_PASS = 'your network/hotspot password'
-BLYNK_AUTH = "your blynk device authentication"
+WIFI_SSID = 'TECNO SPARK 8C'
+WIFI_PASS = 'safder510'
+BLYNK_AUTH = "JzFBEroW2xc5w0Lx4qZzSQrhl4gyGnZ9"
 
 print("Connecting to WiFi network '{}'".format(WIFI_SSID))
 wifi = network.WLAN(network.STA_IF)
